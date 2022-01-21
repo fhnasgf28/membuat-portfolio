19444752ec4c57e0d9a7ab110f8544ad3fb07319
1910631170019_FarhanAssegaf <82705668+fhnasgf28@users.noreply.github.com>
2022-01-21 08:19:00 +0700

    first commit

---
+++ bootstrap-3.3.7-dist/js/script.js
@@ -0,0 +1,9 @@
+// event pada saat link di klik
+
+$(".page-scroll").on("click", function (e) {
+  // ambil isi href link
+
+  var tujuan = $(this).attr("href");
+  // tankap elemen yang bersangkutan
+  console.log(href);
+});
